77489e6d88634a0a9e18c181130c4cc3a0579fcb
Magnus "The Commits" Åberg <himself@magnusaberg.me>
2026-07-24 17:51:51 +0200

    chore(release): v1.4.4

--- src/js/welcome.js
+++ src/js/welcome.js
@@ -1,7 +1,7 @@
 // Welcoming log for browser users
 export function initWelcome() {
     console.log(
-        '%cTjenamors v1.4.3! %c🇸🇪',
+        '%cTjenamors v1.4.4! %c🇸🇪',
         [
             'background: linear-gradient(135deg, #8A2BE2, #4B0082)',
             'color: white',
